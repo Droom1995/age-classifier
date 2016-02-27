11f32b9c67894ed25a2da13ea8eb9e0012a427f8
verbalist <enshteynn@ukr.net>
2016-02-27 18:38:04 +0200

    a

--- data.py
+++ data.py
@@ -14,6 +14,7 @@ from sklearn.preprocessing import Imputer
 
 dataset1 = read_csv('Data/BaseHackathon.csv')
 dataset2 = read_csv('Data/y.csv')
+
 # dataset2 = read_csv('Data/BaseHackathon.csv')
 
 # print(dataset2)
@@ -25,18 +26,39 @@ dataset2 = read_csv('Data/y.csv')
 # dataset = dataset1
 import traceback
 
-imp = Imputer(missing_values='NA', strategy='mean', axis=0)
-imp.fit(dataset1)
+
+
+
 dataset1 = dataset1.select_dtypes(include=[np.number])
+use_field = list(dataset1.columns.values)
+imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imp.fit(dataset1)
+dataset1 = imp.transform(dataset1)
 train = dataset1[:int(len(dataset1)/3*2)]
 test = dataset1[int(len(dataset1)/3*2):]
 target_1 = dataset2.AGE_GROUP1[:int(len(dataset2)/3*2)]
 target_test_1 = dataset2.AGE_GROUP1[int(len(dataset2)/3*2):]
 
-model = ExtraTreesClassifier()
+model = RandomForestClassifier(n_estimators=300)
 model.fit(train, target_1)
 # display the relative importance of each attribute
-print(model.feature_importances_)
+width = 0.35
+feature = model.feature_importances_
+_f = [[x, y] for x, y in zip(use_field, feature)]
+_f.sort(key = lambda x: x[1], reverse=True)
+import openpyxl
+wb = openpyxl.load_workbook('Data/columns_description.xls')
+w = wb.active
+for x in range():
+
+
+feature = [x[1] for x in _f[:15]]
+use_field = [x[0] for x in _f[:15]]
+plt.bar(np.arange(len(feature)), feature, width=0.35)
+plt.xticks(np.arange(len(feature)) + width/2., use_field)
+plt.show()
+
+
 #
 #
 #

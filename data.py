2702e72802d26fbe07cbf2e26ae126caf5621bc4
JobyKK <jobyand121@gmail.com>
2016-02-27 16:52:58 +0200

    init

---
+++ data.py
@@ -0,0 +1,40 @@
+from pandas import read_csv, DataFrame
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import normalize
+import itertools as it
+dataset1 = read_csv('Data/Target_AgeGroup.csv')
+dataset2 = read_csv('Data/BaseHackathon.csv')
+
+print(dataset2)
+
+# merge = dataset2.join(dataset1, on='SUBS_ID')
+
+print(pd.merge(dataset1, dataset2, on='SUBS_ID', how='left'))
+
+# dataset = dataset1
+
+
+#
+# train = dataset[:int(len(dataset)/3*2)]
+# test = dataset[int(len(dataset)/3*2):]
+# target = dataset.Survived[:int(len(dataset)/3*2)]
+# target_test = dataset.Survived[int(len(dataset)/3*2):]
+#
+# X = [
+#      [x if not np.isnan(x) else -1 for x in train.TECH_PRICE_PLAN_BOP]
+# ]
+# X_t = [
+#        [x if not np.isnan(x) else -1 for x in test.TECH_PRICE_PLAN_BOP],
+# ]
+# X_full = [
+#        [x if not np.isnan(x) else -1 for x in dataset.TECH_PRICE_PLAN_BOP],
+# ]
+# # test_predict = [
+# #     [x if not np.isnan(x) else -1 for x in test_dataset.Pclass]
+# # ]
+# X = DataFrame(np.array(X).T)
+# X_t = DataFrame(np.array(X_t).T)
+# X_full = DataFrame(np.array(X_full).T)
+# X_f_t = []
+# # X_f_t = DataFrame(np.array(test_predict).T)
---
+++ train.py
@@ -0,0 +1,33 @@
+from sklearn import metrics
+import seaborn
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn import metrics
+from matplotlib import pyplot as plt
+import numpy as np
+from data import X_t, target, target_test, X \
+    ,X_full, dataset, X_f_t
+
+
+clf = GradientBoostingClassifier(n_estimators=4000, learning_rate=0.001)
+# clf.fit(X, target)
+# print(metrics.classification_report(target_test, clf.predict(X_t)))
+
+
+# clf = RandomForestClassifier(n_jobs=4, n_estimators=10000, min_samples_leaf=3)
+# clf.fit(X, target)
+# print(metrics.classification_report(target_test, clf.predict(X_t)))
+
+clf.fit(X_full, dataset.Survived)
+from csv import DictWriter
+w = DictWriter(open('solve.csv', 'w'), fieldnames=['SUB_ID', 'AGE_GROUP1'])
+w.writeheader()
+for i, x in enumerate(clf.predict(X_f_t)):
+    w.writerow({'SUB_ID': i + 892, 'AGE_GROUP1': x})
+
+width = 0.35
+plt.bar(np.arange(len(clf.feature_importances_)), clf.feature_importances_, width=0.35)
+use_field = ['Pclass']
+plt.xticks(np.arange(len(clf.feature_importances_)) + width/2., use_field)
+
+plt.show()

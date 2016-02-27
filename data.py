09d1fbfc2d601a4c67f4e8f93c2dd92f903d4112
verbalist <enshteynn@ukr.net>
2016-02-27 22:26:01 +0200

    a

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import normalize
 import itertools as it
-from sklearn import metrics
+from sklearn import metrics, preprocessing
 import seaborn
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, ExtraTreesClassifier
@@ -11,17 +11,17 @@ from sklearn import metrics
 from matplotlib import pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import Imputer
+from throw_outliers import throw_outliers
+dataset = read_csv('Data/BaseHackathon.csv')
+# dataset2 = read_csv('Data/y.csv')
 
-dataset1 = read_csv('Data/BaseHackathon.csv')
-dataset2 = read_csv('Data/y.csv')
-
-# dataset2 = read_csv('Data/BaseHackathon.csv')
+dataset2 = read_csv('Data/Target_AgeGroup.csv')
 
 # print(dataset2)
 
 # merge = dataset2.join(dataset1, on='SUBS_ID')
 
-# m = pd.merge(dataset1, dataset2, on='SUBS_ID', how='left')
+
 # m.to_csv(open('Data/X.csv', 'w'))
 # dataset = dataset1
 import traceback
@@ -29,53 +29,71 @@ import traceback
 
 
 
-dataset1 = dataset1.select_dtypes(include=[np.number])
+dataset1 = dataset.select_dtypes(include=[np.number])
 use_field = list(dataset1.columns.values)
 imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imp.fit(dataset1)
 dataset1 = imp.transform(dataset1)
+dataset1 = DataFrame(dataset1, columns=use_field)
+dataset1 = throw_outliers(dataset1)
+dataset1 = dataset1.assign(SUBS_ID=dataset.SUBS_ID)
+dataset1 = pd.merge(dataset1, dataset2, on='SUBS_ID', how='left')
+# print(use_field)
+dataset = dataset1.groupby('SUBS_ID').mean()
+dataset1 = dataset.copy()
+dataset1 = dataset1.drop(['AGE_GROUP1', 'AGE_GROUP2'], axis=1)
+# dataset1 = dataset1['SUBS_ID']
+      # (dataset.columns.values[0]))
+# dataset1 = preprocessing.scale(dataset1)
+#
 train = dataset1[:int(len(dataset1)/3*2)]
 test = dataset1[int(len(dataset1)/3*2):]
-target_1 = dataset2.AGE_GROUP1[:int(len(dataset2)/3*2)]
-target_test_1 = dataset2.AGE_GROUP1[int(len(dataset2)/3*2):]
+target_1 = dataset.AGE_GROUP1[:int(len(dataset2)/3*2)]
+target_test_1 = dataset.AGE_GROUP1[int(len(dataset2)/3*2):]
 
-model = RandomForestClassifier(n_estimators=300)
+model = RandomForestClassifier(n_estimators=300, n_jobs=5)
 model.fit(train, target_1)
+print(metrics.classification_report(target_test_1, model.predict(test)))
 # display the relative importance of each attribute
-width = 0.35
-feature = model.feature_importances_
-_f = [[x, y] for x, y in zip(use_field, feature)]
-_f.sort(key = lambda x: x[1], reverse=True)
-import openpyxl
-wb = openpyxl.load_workbook('Data/columns_description.xls')
-w = wb.active
-
-feature = [x[1] for x in _f[:15]]
-use_field = [x[0] for x in _f[:15]]
-plt.bar(np.arange(len(feature)), feature, width=0.35)
-plt.xticks(np.arange(len(feature)) + width/2., use_field)
-plt.show()
-
-
+# # width = 0.35
+# # feature = model.feature_importances_
 #
-#
-#
-#
+# # _f = [[x, y] for x, y in zip(use_field, feature)]
+# # _f = {x: y for x, y in zip(use_field, feature)}
+# # _f.sort(key = lambda x: x[1], reverse=True)
+# # import openpyxl
+# # wb = openpyxl.load_workbook('Data/columns_description.xlsx')
+# # w = wb.active
+# # for x in range(2, 178):
+# #     w['D%s' % (x + 2)] = _f.get(w['B%s' % (x + 2)].value, -1)*100
+# # wb.save('Data/columns_description.xlsx')
 # #
-# # X = [
-# #      [x if not np.isnan(x) else -1 for x in train.TECH_PRICE_PLAN_BOP]
-# # ]
-# # X_t = [
-# #        [x if not np.isnan(x) else -1 for x in test.TECH_PRICE_PLAN_BOP],
-# # ]
-# # X_full = [
-# #        [x if not np.isnan(x) else -1 for x in dataset.TECH_PRICE_PLAN_BOP],
-# # ]
-# # # test_predict = [
-# # #     [x if not np.isnan(x) else -1 for x in test_dataset.Pclass]
+# # feature = [x[1] for x in _f[:15]]
+# # use_field = [x[0] for x in _f[:15]]
+# # plt.bar(np.arange(len(feature)), feature, width=0.35)
+# # plt.xticks(np.arange(len(feature)) + width/2., use_field)
+# # plt.show()
+# #
+# #
+# # #
+# # #
+# # #
+# # #
+# # #
+# # # X = [
+# # #      [x if not np.isnan(x) else -1 for x in train.TECH_PRICE_PLAN_BOP]
 # # # ]
-# # X = DataFrame(np.array(X).T)
-# # X_t = DataFrame(np.array(X_t).T)
-# # X_full = DataFrame(np.array(X_full).T)
-# # X_f_t = []
-# # # X_f_t = DataFrame(np.array(test_predict).T)
+# # # X_t = [
+# # #        [x if not np.isnan(x) else -1 for x in test.TECH_PRICE_PLAN_BOP],
+# # # ]
+# # # X_full = [
+# # #        [x if not np.isnan(x) else -1 for x in dataset.TECH_PRICE_PLAN_BOP],
+# # # ]
+# # # # test_predict = [
+# # # #     [x if not np.isnan(x) else -1 for x in test_dataset.Pclass]
+# # # # ]
+# # # X = DataFrame(np.array(X).T)
+# # # X_t = DataFrame(np.array(X_t).T)
+# # # X_full = DataFrame(np.array(X_full).T)
+# # # X_f_t = []
+# # # # X_f_t = DataFrame(np.array(test_predict).T)
--- throw_outliers.py
+++ throw_outliers.py
@@ -4,49 +4,49 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import Imputer
 import matplotlib.pyplot as plt
 
-def throw_outliers(data):
-    # create the training + test sets
-    try:
-        data = pd.read_csv('Data/BaseHackathon.csv')
-    except IOError:
-        print("io ERROR-->Could not locate file.")
+def throw_outliers(data, perc=75):
+    """ create the training + test sets"""
 
-    dataset1 = data.select_dtypes(include=[np.number])
-    use_field = list(dataset1.columns.values)
-    imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
-    imp.fit(dataset1)
-    data = imp.transform(dataset1)
-    data = pd.DataFrame(data)
-    # data = data[:10000] # for test
-    numerics_cols = data.select_dtypes(include=[np.number])
-    all_cols = set(data.columns)
-    quantity_cols = set(numerics_cols.columns)
-
-    quality_cols = all_cols - quantity_cols
-
-    #quantity_cols = list(quantity_cols)[:] # for test
-
+    numerics_cols = data.select_dtypes(include=[np.number]).columns.values
     quantity_cols_3devs = {}
     quantity_cols_95percentiles = {}
     quantity_cols_5percentiles = {}
+    quantity_cols_mean = {}
     old_data = {}
-    for quan_col in quantity_cols:
-        current_col = data[quan_col]
 
+    for quan_col in numerics_cols:
+        current_col = data[quan_col]
         quantity_cols_3devs[quan_col] = current_col.std()*3
-        quantity_cols_95percentiles[quan_col] = np.percentile(current_col,75)
-        quantity_cols_5percentiles[quan_col] = np.percentile(current_col,25)
+
+        quantity_cols_95percentiles[quan_col] = np.percentile(current_col, perc)
+        quantity_cols_5percentiles[quan_col] = np.percentile(current_col, 100 - perc)
+        quantity_cols_mean[quan_col] = current_col.mean()
         old_data[quan_col] = current_col #len(current_col[abs(current_col) > current_col.std()*3])
 
-
     for index, row in data.iterrows():
-        for col in quantity_cols:
-            if row[col] > quantity_cols_3devs[col]:
+        for col in numerics_cols:
+            if row[col] > quantity_cols_mean[col]+quantity_cols_3devs[col]:
                 row[col] = quantity_cols_95percentiles[col]
-            elif row[col] < quantity_cols_3devs[col]:
+            elif row[col] < quantity_cols_mean[col]-quantity_cols_3devs[col]:
                 row[col] = quantity_cols_5percentiles[col]
 
-    for quan_col in quantity_cols:
+    for quan_col in numerics_cols:
         current_col = data[quan_col]
         print(old_data[quan_col], ' ', len(current_col[abs(current_col) > quantity_cols_3devs[col]]))
     return data
+
+
+if __name__ == '__main__':
+    import time
+    from pandas import read_csv, DataFrame
+    t = time.time()
+    dataset1 = read_csv('Data/BaseHackathon.csv')
+    dataset1 = dataset1.head(1000)
+    dataset1 = dataset1.select_dtypes(include=[np.number])
+    use_field = list(dataset1.columns.values)
+    imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    imp.fit(dataset1)
+    dataset1 = imp.transform(dataset1)
+    dataset1 = DataFrame(dataset1, columns=use_field)
+    throw_outliers(dataset1)
+    print(float(time.time() - t)*100/60)
